96a372464dfc5ba2815457ad0da8631dd2cda09c
airandember <air@airandember.com>
2025-05-09 10:45:57 -0600

    home mobile menu for portrait and landscapes

--- scripts/mobMenu.js
+++ scripts/mobMenu.js
@@ -1,8 +1,10 @@
 const menuDiv = document.querySelector('.mobileMenu');
 const menuTitle = document.getElementById('mobMenuTitle');
+const mobileCarrot = document.getElementById('mobMenuCarrot');
 
 const toggleMenu = () => {
     menuDiv.classList.toggle('mobileOpen');
 };
 
 menuTitle.addEventListener('click', toggleMenu);
+mobileCarrot.addEventListener('click', toggleMenu);
