40038294d86416ab49768f1d48777130f65327bc
airandember <air@airandember.com>
2025-11-04 13:42:37 -0700

    Gift Cards additions!!

---
+++ scripts/headfoot.js
@@ -0,0 +1,28 @@
+fetch('../pages/header.html')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to load header.html');
+    }
+    return response.text();
+  })
+  .then(data => {
+    document.getElementById('header').innerHTML = data;
+  })
+  .catch(error => {
+    console.error('Error loading header:', error);
+  });
+
+
+fetch('../pages/footer.html')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to load footer.html');
+    }
+    return response.text();
+  })
+  .then(data => {
+    document.getElementById('footer').innerHTML = data;
+  })
+  .catch(error => {
+    console.error('Error loading footer:', error);
+  });
